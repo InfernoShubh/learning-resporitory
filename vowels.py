25a449ab22efcc864ffe83e9823126ca0aa92b60
Shubh <shubham.parida79@gmail.com>
2026-07-14 23:01:03 +0530

    Add files via upload

---
+++ vowels.py
@@ -0,0 +1,6 @@
+character=input("character:")
+vowels=["A","E","I","O","U","a","e","i","o","u"]
+if character in vowels:
+    print("character is a vowel")
+else:
+    print("character is a consonant")
