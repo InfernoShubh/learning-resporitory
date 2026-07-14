1bb475283fd3f268e40c314007dd59075cd8ec7a
Shubh <shubham.parida79@gmail.com>
2026-07-14 23:02:36 +0530

    Add files via upload

---
+++ tup.py
@@ -0,0 +1,7 @@
+tup1=(2,2.5,"hello")
+tup2=("HELLO",2.333,6)
+print(type(tup1))
+print(type(tup2))
+print(tup1[1:])#slicing
+print(tup1+tup2)#concention using +operator
+print(tup2*4)#repeatition using *operator
