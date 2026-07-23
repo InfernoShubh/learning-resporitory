00a27ab74859b69374d914891cb7d6c8468415b9
Shubh <shubham.parida79@gmail.com>
2026-07-23 20:20:41 +0530

    Add files via upload

---
+++ format 3.py
@@ -0,0 +1,2 @@
+detail="name={0},age={1},course={2}".format("shubham","17","data science")
+print(detail)
---
+++ format swapping.py
@@ -0,0 +1,7 @@
+x=89
+y=90
+temp=x
+x=y
+y=temp
+print("value after swapping x={}".format(x))
+print("value after swapping y={}".format(y))
