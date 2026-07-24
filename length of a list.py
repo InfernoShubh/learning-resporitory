bee366d338a16fea465ef7991ad7b21adb2e4867
Shubh <shubham.parida79@gmail.com>
2026-07-24 22:20:34 +0530

    Add files via upload

---
+++ length of a list.py
@@ -0,0 +1,7 @@
+s=[2,34,23,4]
+b=len(s)#simple way to find len using len()
+c=0
+for i in s:#length of a list using loop
+    c+=1
+print(c)
+print(b)
---
+++ list.py
@@ -0,0 +1,8 @@
+list=[2,"hello",4.98,"python"]
+list2=["world",8.990,99]
+print(list)
+print(list2)
+print(list[1:4])
+print(list2[2:])
+print(list+list2)
+print(list2*5)
