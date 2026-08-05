4e828bfea3fe171092eda63c78beb47c2aeb7767
Shubh <shubham.parida79@gmail.com>
2026-08-05 18:18:33 +0530

    Add files via upload

---
+++ ele dl.py
@@ -0,0 +1,7 @@
+a=int(input("age:"))
+if (a>=16):
+    print("eligible to drive upto 50cc")
+else:print("not eligibe to drive")
+if (a>=18):
+    print("eligible to drive cars and motorcycles as well")
+else:print("not eligile to drive cars and motorcyles as well")
