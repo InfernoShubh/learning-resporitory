f1f4bce8563272c2429e56194fd0fb557b77375e
Shubh <shubham.parida79@gmail.com>
2026-08-15 07:32:36 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,16 @@
+num1=int( input(""))
+operation=input("")
+num2=int(input(""))
+print(num1,operation,num2)
+if operation== "+":
+    print(num1+num2)
+elif operation== "-":
+    print(num1-num2)
+elif operation== "*":
+    print(num1*num2)
+elif operation== "/":
+    print(num1/num2)
+elif operation== "%":
+    print(num1%num2)
+
+    
