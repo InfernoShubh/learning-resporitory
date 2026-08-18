500ad71bddcbe1b8faa219f3afd526b52e63a6eb
Shubh <shubham.parida79@gmail.com>
2026-08-18 23:16:57 +0530

    Add files via upload

---
+++ append,expend,insert in list.py
@@ -0,0 +1,7 @@
+a=[2,34,5,67,39]
+b=[34,34,4,3]
+b.append(a)#this add a list as an single element
+b.extend(a)#this add the list individualy
+print(b)  
+a.insert(2 ,84)# insert use for adding to a particular index
+print(a)
