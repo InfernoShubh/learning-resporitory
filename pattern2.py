3e5d038e6066e47e8ec1dfd0641dd5e0f9c79681
Shubh <shubham.parida79@gmail.com>
2026-08-27 22:14:50 +0530

    Add files via upload

---
+++ pattern2.py
@@ -0,0 +1,9 @@
+for i in range(0,5):
+    for j in range(0,5):
+        if i==0 or i==5-1 or j==0 or j==4:
+            print("*",end=" ")
+        elif j==2:
+            print('@',end=" ")
+        else:
+            print("  ",end="")
+    print()
